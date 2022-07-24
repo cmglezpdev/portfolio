c2f96bc5f735942d66830c9a306a90309a1cb783
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-07-24 13:31:48 -0400

    add progess bar

--- src/components/buttons/Progress.jsx
+++ src/components/buttons/Progress.jsx
@@ -3,6 +3,18 @@ import './progress.scss';
 
 export const Progress = () => {
   return (
-    <div>Progress</div>
+    <div className='progress'>
+      
+      <div className="squares">
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+      </div>
+
+      <div className="vertical-line"></div>
+
+    </div>
   )
 }
