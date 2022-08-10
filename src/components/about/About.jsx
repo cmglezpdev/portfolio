cc2192e801db799dfb0e352c3b978511eb92c594
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-08-09 22:49:27 -0400

    change description about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -18,10 +18,10 @@ export const About = () => {
                 <div className="description">
                     <p>
                         Hola, mi nombre es Carlos Manuel González y soy estudiante de Ciencias de la Computación en la Universidad de la Habana. <br />
-                        Me apasiona el mundo del desarrollo de softweare en general, aunque actualmente estoy enfocado en el desarrollo web. <br />
+                        Me apasiona el mundo del desarrollo web, las criptomonedas y el machine learning, aunque actualmente estoy enfocado en el desarrollo web. <br />
                         Empecé a programar a los 16 años en la parte de programación competitiva. Estube 3 años enfocado solo en eso y participé en muchas competencias tanto nacionales como internacionales desde mi preuniversitario. <br />
 
-                        Cuando entré en la universidad emprecé a interesarme mas por el desarrollo web, llegando a desarrollar aplicaciones tanto frontend como de backend. Me interesa mucho el desarrollo web, las criptomonedas y el machine learning. <br />
+                        Cuando entré en la universidad emprecé a interesarme mas por el desarrollo web, llegando a desarrollar aplicaciones tanto frontend como de backend. <br />
                         No he tenido todavía experiencia laboral pero si sigo creando aplicaciones personales como parte de mi aprendizaje, y estoy abierto a poder trabajar y colaborar con una empresa u otro desarrollador que quiera trabajar conmigo. <br />
                     </p>
                 </div>
