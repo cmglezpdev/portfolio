c2914536beb2a3d13fd86ce448136adf46962f0b
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-15 11:01:20 -0400

    feat: add link in my email in the contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -9,7 +9,7 @@ export const Contact = () => {
 
       <Title text={"Contact"}/>
 
-      <p className='contact-text'>I'm available for new projects and work offers, so if you are interested in my work and want that work us together, or simply want that i create the application that you desire, you can write to this email cmglezpdev@gmail.com and we talk it.</p>
+      <p className='contact-text'>I'm available for new projects and work offers, so if you are interested in my work and want that work us together, or simply want that i create the application that you desire, you can write to this email <a href='maito:cmglezpdev@gmail.com'>cmglezpdev@gmail.com</a> and we talk it.</p>
     </div>
   )
 }
