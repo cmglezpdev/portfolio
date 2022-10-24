7f2d16b9e31ea79ca95659f97d97be16c25ba0aa
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-23 20:38:22 -0400

    feat: add mark to the incomplete projects

---
+++ src/components/Work/Mark.jsx
@@ -0,0 +1,12 @@
+
+import './mark.scss';
+
+export const Mark = () => {
+    return (
+        <div className="message">
+            <span>
+                CURRENTLY WORKING ON
+            </span>
+        </div>
+    )
+}
