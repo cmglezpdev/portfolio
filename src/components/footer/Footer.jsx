e1340067407a696d971677489692a0d9c9b8b942
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-07-12 16:19:38 -0400

    fix(): update built date in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ import './footer.scss'
 export const Footer = () => {
   return (
     <footer className="footer-container">
-        <span className="copyright">© 2022 - Built by Carlos Manuel</span>
+        <span className="copyright">© {new Date().getFullYear()} - Built by Carlos Manuel</span>
 
         <div className="social">
             <a href='https://github.com/cmglezpdev' target="_blank" rel='noreferrer'><AiFillGithub /></a>
